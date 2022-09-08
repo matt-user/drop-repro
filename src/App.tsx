f0cb1af83d965ecf881bfbe8d8948974549cbb97
Matt Auer <mattauer@Matts-MacBook-Pro.local>
2022-09-08 14:44:30 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+import { Dropdown, Button } from "@fuel-ui/react";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Dropdown>
+        <Dropdown.Trigger>
+          <Button>User to favor</Button>
+        </Dropdown.Trigger>
+        <Dropdown.Menu autoFocus aria-label="Actions">
+          <Dropdown.MenuItem key="seller" textValue="Seller">
+            Seller
+          </Dropdown.MenuItem>
+          <Dropdown.MenuItem key="buyer" textValue="Buyer">
+            Buyer
+          </Dropdown.MenuItem>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  )
+}
+
+export default App
